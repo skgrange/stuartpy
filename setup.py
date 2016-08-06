f37c6ccdfdf51ec8aeac2ddc3007ab7f1a5b3dab
skgrange <s.k.grange@gmail.com>
2016-08-06 20:10:01 +0100

    add pretty_json function and do a little tidying up

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
-setup(name = 'stuartpy',
-      version = '0.1.4',
-      description = 'Things',
-      url = 'http://github.com/skgrange',
-      author = 'Stuart K. Grange',
-      author_email = '',
-      license = 'MIT',
-      packages = ['stuartpy'],
+setup(name = "stuartpy",
+      version = "0.1.5",
+      description = "Things",
+      url = "http://github.com/skgrange",
+      author = "Stuart K. Grange",
+      author_email = "",
+      license = "MIT",
+      packages = ["stuartpy"],
       install_requires = ["xmltodict"],
       scripts = ["bin/restart_network_connection", "bin/dropbox_sync", 
         "bin/pretty_xml", "bin/xml_to_json", "bin/lights_out", 
-        "bin/pdf_drop_first_page", "bin/dropbox_start"],
+        "bin/pdf_drop_first_page", "bin/dropbox_start", "bin/pretty_json"],
       zip_safe = False)
-
